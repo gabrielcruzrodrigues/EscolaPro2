8bccc96cdcb7d5340e9a52278df53f7d16fb8171
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2025-04-23 11:04:57 -0300

    feat: Finalizando o endpoint de Update no FamilyController

--- EscolaPro/Controllers/FamilyController.cs
+++ EscolaPro/Controllers/FamilyController.cs
@@ -1,8 +1,10 @@
 ﻿using EscolaPro.Models;
+using EscolaPro.Models.Dtos;
 using EscolaPro.Repositories.Interfaces;
 using EscolaPro.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
 
 namespace EscolaPro.Controllers;
@@ -191,6 +193,143 @@ public class FamilyController : ControllerBase
 
         var companie = await _familyRepository.Search(userCompanie.Name, param);
         return Ok(companie);
+    }
+
+    [HttpPut]
+    [Authorize(policy: "admin_internal")]
+    public async Task<ActionResult> Update(UpdateFamilyViewModel request)
+    {
+        // ============= Início validação de empresa e adquirimento do nome da empresa =============
+
+        var userId = long.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+        var userCompanieId = int.Parse(User.FindFirst("CompanieId")?.Value);
+
+        if (!await CompanieValidation(userId, userCompanieId))
+        {
+            return BadRequest("Você não tem acesso a essa empresa!");
+        }
+        var userCompanie = await _companieRepository.GetByIdAsync(userCompanieId);
+
+        // ============= Fim validação de empresa e adquirimento do nome da empresa =============
+
+        var family = await _familyRepository.GetByIdAsync(userCompanie.Name, request.Id);
+
+        //Validar alteração de imagem aqui
+
+        if (!request.Name.IsNullOrEmpty())
+        {
+            if (await _familyRepository.GetByNameAsync(userCompanie.Name, request.Name) == null)
+            {
+                family.Name = request.Name;
+            }
+            else
+            {
+                return BadRequest("Este Nome já está cadastrado no banco de dados!");
+            }
+        }
+
+        if (!request.Email.IsNullOrEmpty())
+        {
+            if (await _familyRepository.GetByEmailAsync(userCompanie.Name, request.Email) == null)
+            {
+                family.Email = request.Email;
+            }
+            else
+            {
+                return BadRequest("Este Email já está cadastrado no banco de dados!");
+            }
+        }
+        
+
+        if (!request.Rg.IsNullOrEmpty())
+        {
+            if (await _familyRepository.GetByRgAsync(userCompanie.Name, request.Rg) == null)
+            {
+                family.Rg = request.Rg;
+            }
+            else
+            {
+                return BadRequest("Este RG já está cadastrado no banco de dados!");
+            }
+        }
+
+        if (!request.Cpf.IsNullOrEmpty())
+        {
+            if (await _familyRepository.GetByCpfAsync(userCompanie.Name, request.Cpf) == null)
+            {
+                family.Cpf = request.Cpf;
+            }
+            else
+            {
+                return BadRequest("Este CPF já está cadastrado no banco de dados!");
+            }
+        }
+        
+
+        if (!request.Phone.IsNullOrEmpty())
+        {
+            if (await _familyRepository.GetByPhoneAsync(userCompanie.Name, request.Phone) == null)
+            {
+                family.Phone = request.Phone;
+            }
+            else
+            {
+                return BadRequest("Este Telefone já está cadastrado no banco de dados!");
+            }
+        }
+        
+
+        if (request.Role != null)
+        {
+            if (Enum.IsDefined(typeof(Roles), request.Role))
+            {
+                family.Role = request.Role.Value;
+            }
+            else
+            {
+                return BadRequest("Essa Role não está cadastrada no banco de dados!");
+            }
+        }
+        
+
+        if (request.Type != null)
+        {
+            if (Enum.IsDefined(typeof(Type), request.Type))
+            {
+                family.Type = request.Type.Value;
+            }
+            else
+            {
+                return BadRequest("Esse tipo não está cadastrado no banco de dados!");
+            }
+        }
+
+        if (request.StudentId != null)
+        {
+            if (await _studentRepository.GetbyIdAsync(userCompanie.Name, request.StudentId.Value) != null)
+            {
+                family.StudentId = request.StudentId.Value;
+            }
+            else
+            {
+                return BadRequest("Esse tipo não está cadastrado no banco de dados!");
+            }
+        }
+
+        family.DateOfBirth = request.DateOfBirth ?? family.DateOfBirth;
+        family.Nationality = request.Nationality ?? family.Nationality;
+        family.Naturalness = request.Naturalness ?? family.Naturalness;
+        family.Sex = request.Sex ?? family.Sex;
+        family.Cep = request.Cep ?? family.Cep;
+        family.Address = request.Address ?? family.Address;
+        family.Neighborhood = request.Neighborhood ?? family.Neighborhood;
+        family.City = request.City ?? family.City;
+        family.State = request.State ?? family.State;
+        family.WorkAddress = request.WorkAddress ?? family.WorkAddress;
+        family.Ocupation = request.Ocupation ?? family.Ocupation;
+
+        await _familyRepository.Update(userCompanie.Name, family);
+        return NoContent();
     }
 
     private async Task<bool> CompanieValidation(long userId, int companieId)
---
+++ EscolaPro/ViewModels/UpdateFamilyViewModel.cs
@@ -0,0 +1,10 @@
+﻿using EscolaPro.Models.Dtos;
+namespace EscolaPro.ViewModels;
+
+public class UpdateFamilyViewModel : UpdateUserInternalViewModel
+{
+    public string? WorkAddress { get; set; }
+    public string? Ocupation { get; set; }
+    public FamilyType? Type { get; set; }
+    public long? StudentId { get; set; }
+}
